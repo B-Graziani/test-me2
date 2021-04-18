1f14b07497aad74bc516910ce732d3861e1d5661
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-18 20:35:01 +0200

    done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,40 +1,49 @@
 class RestaurantsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
-    @restaurants = Restaurant.all
+    @restaurants = policy_scope(Restaurant)
   end
   def show
     @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant
   end
   def chef_name
     @restaurant = Restaurant.find(params[:id])
   end
   def top
     @restaurants = Restaurant.where(stars: 5)
+    authorize @restaurant
   end
   def new
     @restaurant = Restaurant.new
+    authorize @restaurant
   end
   def create
     @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+
     if @restaurant.save
       redirect_to @restaurant
     else
       render :new
     end
+    authorize @restaurant
   end
   def edit
     @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant
   end
   def update
     @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
+    authorize @restaurant
     redirect_to @restaurant
   end
   def destroy
     @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
     redirect_to restaurants_path
+    authorize @restaurant
   end
 
   private
