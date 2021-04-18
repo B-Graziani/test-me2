26d54e876bd4cf856f657c3aaef04454a1076a44
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-18 15:41:52 +0200

    e

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'restaurants#index'
   resources :restaurants, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     member do
