b4e52557b9bf15e9ff89a170871849f0468fe87b
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-18 15:30:23 +0200

    done 1st part

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     collection do
       get :top
     end
+    resources :reviews, only: [:new, :create]
   end
+  resources :reviews, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
